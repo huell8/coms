af4eeb2fc4bd15134bdfdf723384f565ec966d71
Huell Babineaux <70027895+huell8@users.noreply.github.com>
2022-11-16 09:25:18 +0100

    added support for wss

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
+# client.py
 
 import asyncio
 import websockets
+import ssl
+import pathlib
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+localhost_pem = pathlib.Path(__file__).with_name("cert.pem")
+ssl_context.load_verify_locations(localhost_pem)
 
 async def hello(uri):
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri, ssl=ssl_context) as websocket:
         await websocket.send("ping")
         message = await websocket.recv()
         print(message)
 
-asyncio.run(hello("ws://localhost:9696"))
+asyncio.run(hello("wss://localhost:9696"))
--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
+# server.py
 
 import asyncio
-from websockets import serve
+import websockets
+import ssl
+import pathlib
 
 async def echo(websocket):
-    async for message in websocket:
-        if message == "ping":
-            await websocket.send("pong")
+    message = await websocket.recv()
+    await websocket.send("pong")
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("cert.pem")
+ssl_context.load_cert_chain(localhost_pem)
 
 async def main():
-    async with serve(echo, "localhost", 9696):
+    async with websockets.serve(echo, "localhost", 9696, ssl=ssl_context):
         await asyncio.Future()
 
 if __name__ == "__main__":
